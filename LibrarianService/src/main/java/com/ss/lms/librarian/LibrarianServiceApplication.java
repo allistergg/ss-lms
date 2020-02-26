1ba1546810b23f16cb2846bd7bc2c72c8574cf8c
ace <allisterg@gmail.com>
2020-02-26 00:15:03 -0500

    add entities to jar, make application scan entity in jar

--- LibrarianService/src/main/java/com/ss/lms/librarian/LibrarianServiceApplication.java
+++ LibrarianService/src/main/java/com/ss/lms/librarian/LibrarianServiceApplication.java
@@ -2,8 +2,10 @@ package com.ss.lms.librarian;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.smoothstack.lms.administrator.model")
 public class LibrarianServiceApplication {
 
 	public static void main(String[] args) {
