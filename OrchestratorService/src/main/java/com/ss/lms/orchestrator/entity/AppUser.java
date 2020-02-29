70de2ff1d8a5b7f7226463ca6fb4c4a07ab05f17
ace <allisterg@gmail.com>
2020-02-28 21:02:38 -0500

    jwt

---
+++ OrchestratorService/src/main/java/com/ss/lms/orchestrator/entity/AppUser.java
@@ -0,0 +1,37 @@
+package com.ss.lms.orchestrator.entity;
+
+public class AppUser {
+	
+	private String username;
+	private String password;
+
+
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	@Override
+	public String toString() {
+		return "AppUser [username=" + username + ", password=" + password;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
